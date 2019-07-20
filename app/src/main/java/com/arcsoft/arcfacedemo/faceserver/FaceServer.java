3b3c89c929d07c1d50a9077c4befda01e911eb59
wangshengyang <351275088@qq.com>
2019-07-20 11:40:56 +0800

    【功能修改】 修改getBestRect函数

--- app/src/main/java/com/arcsoft/arcfacedemo/faceserver/FaceServer.java
+++ app/src/main/java/com/arcsoft/arcfacedemo/faceserver/FaceServer.java
@@ -297,11 +297,11 @@ public class FaceServer {
     /**
      * 用于注册照片人脸
      *
-     * @param context  上下文对象
-     * @param bgr24    bgr24数据
-     * @param width    bgr24宽度
-     * @param height   bgr24高度
-     * @param name     保存的名字，若为空则使用时间戳
+     * @param context 上下文对象
+     * @param bgr24   bgr24数据
+     * @param width   bgr24宽度
+     * @param height  bgr24高度
+     * @param name    保存的名字，若为空则使用时间戳
      * @return 是否注册成功
      */
     public boolean registerBgr24(Context context, byte[] bgr24, int width, int height, String name) {
@@ -447,47 +447,29 @@ public class FaceServer {
      * @param srcRect 原Rect
      * @return 调整后的Rect
      */
+
     private static Rect getBestRect(int width, int height, Rect srcRect) {
 
         if (srcRect == null) {
             return null;
         }
         Rect rect = new Rect(srcRect);
-        //1.原rect边界已溢出宽高的情况
-        int maxOverFlow = 0;
-        int tempOverFlow = 0;
-        if (rect.left < 0) {
-            maxOverFlow = -rect.left;
-        }
-        if (rect.top < 0) {
-            tempOverFlow = -rect.top;
-            if (tempOverFlow > maxOverFlow) {
-                maxOverFlow = tempOverFlow;
-            }
-        }
-        if (rect.right > width) {
-            tempOverFlow = rect.right - width;
-            if (tempOverFlow > maxOverFlow) {
-                maxOverFlow = tempOverFlow;
-            }
-        }
-        if (rect.bottom > height) {
-            tempOverFlow = rect.bottom - height;
-            if (tempOverFlow > maxOverFlow) {
-                maxOverFlow = tempOverFlow;
-            }
-        }
-        if (maxOverFlow != 0) {
+        int maxOverFlow = Math.max(-rect.left, Math.min(-rect.top, Math.min(width - rect.right, height - rect.bottom)));
+        // 原rect边界已溢出宽高的情况
+        if (maxOverFlow > 0) {
             rect.left += maxOverFlow;
             rect.top += maxOverFlow;
             rect.right -= maxOverFlow;
             rect.bottom -= maxOverFlow;
             return rect;
         }
-        //2.原rect边界未溢出宽高的情况
+        // 原rect边界未溢出宽高的情况
         int padding = rect.height() / 2;
-        //若以此padding扩张rect会溢出，取最大padding为四个边距的最小值
-        if (!(rect.left - padding > 0 && rect.right + padding < width && rect.top - padding > 0 && rect.bottom + padding < height)) {
+        // 若以此padding扩张rect会溢出，取最大padding为四个边距的最小值
+        if (!(rect.left - padding > 0
+                && rect.right + padding < width
+                && rect.top - padding > 0
+                && rect.bottom + padding < height)) {
             padding = Math.min(Math.min(Math.min(rect.left, width - rect.right), height - rect.bottom), rect.top);
         }
 
